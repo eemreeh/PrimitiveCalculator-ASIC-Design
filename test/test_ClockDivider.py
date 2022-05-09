5b0e5f7915f3202e2201285940c86fe709f228eb
Matt Venn <matt@mattvenn.net>
2022-05-09 10:58:18 +0200

    make clock divider a better test

--- test/test_ClockDivider.py
+++ test/test_ClockDivider.py
@@ -1,14 +1,28 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
 
 
+# test the default case, with reg len set to 8, so 128 cycles of off then 128 cycles of on
 @cocotb.test()
 async def test_ClockDivider(dut):
-    dut.rst.value = 1
-    await Timer(3, units="ns")
     
-    for cycle in range(20):
-        dut.clk.value = 0
-        await Timer(1, units="ns")
-        dut.clk.value = 1
-        await Timer(1, units="ns")
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.fork(clock.start())
+
+    dut.rst.value = 1
+    await ClockCycles(dut.clk, 5)
+    dut.rst.value = 0
+
+    # repeat 4 times
+    for j in range(4):
+
+        # starts low
+        for i in range(128):
+            await ClockCycles(dut.clk, 1)
+            assert(dut.out == 0)
+
+        # goes high
+        for i in range(128):
+            await ClockCycles(dut.clk, 1)
+            assert(dut.out == 1)
